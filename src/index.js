56b25e2b59bd0be726d470e81c4f6aca512ba6e7
Simona <see.moona@yahoo.com>
2020-04-22 16:01:38 +0200

    1st version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const addList = () => {
     let userInput = document.querySelector("#userText").value;
     console.log(userInput);
@@ -7,4 +6,25 @@ const addList = () => {
     newElem.appendChild(textLine);
     document.querySelector(".result").appendChild(newElem);
     document.querySelector("#userText").value = "";
+
+
+const colorGen=()=>{
+    let result = "#"
+    let colorCode= "0123456789ABCDEF";
+    for (let i = 0; i < 6; i++) {
+       result += colorCode[Math.floor(Math.random()*16)];
+        
+    }
+    return result;
+}
+//console.log(colorGen());
+
+
+let items = document.querySelectorAll("li");
+console.log(items);
+
+items.forEach((item) => {
+    item.style.color = colorGen();  
+    
+});
 }
