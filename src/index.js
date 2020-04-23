bd806acd916be58c9f25343e3598d73395a70aa9
Simona <see.moona@yahoo.com>
2020-04-23 13:30:22 +0200

    closing button added

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
+const colorGen = () => {
+  let result = "#";
+  let colorCode = "0123456789ABCDEF";
+  for (let i = 0; i < 6; i++) {
+    result += colorCode[Math.floor(Math.random() * 16)];
+  }
+  return result;
+};
+
 const addList = () => {
-    let userInput = document.querySelector("#userText").value;
-    console.log(userInput);
-    let newElem = document.createElement("li");
-    let textLine = document.createTextNode(userInput);
-    newElem.appendChild(textLine);
-    document.querySelector(".result").appendChild(newElem);
-    document.querySelector("#userText").value = "";
+  let userInput = document.querySelector("#userText").value;
+  if (userInput == "") {
+    return alert("You cannot have an empty task");
+  }
+  let newElem = document.createElement("li");
+  let textLine = document.createTextNode(userInput);
+  newElem.appendChild(textLine);
+  document.querySelector(".result").appendChild(newElem);
+  document.querySelector("#userText").value = "";
+  let items = document.querySelectorAll("li");
+
+  let span = document.createElement("span");
+  let closeButton = document.createTextNode("X");
+  span.className = "close";
+  span.appendChild(closeButton);
+  items.forEach((item) => {
+    item.style.color = colorGen();
+    item.appendChild(span);
+  });
 
 
-const colorGen=()=>{
-    let result = "#"
-    let colorCode= "0123456789ABCDEF";
-    for (let i = 0; i < 6; i++) {
-       result += colorCode[Math.floor(Math.random()*16)];
-        
-    }
-    return result;
-}
-//console.log(colorGen());
-
-
-let items = document.querySelectorAll("li");
-console.log(items);
-
-items.forEach((item) => {
-    item.style.color = colorGen();  
+  
+Object.assign(span, {
+  onclick: function () {
+    span.classList.add("invisible");
+   // span.style.display = "none";
     
-});
-}
+  }
+ 
+})
+
+
+ };
